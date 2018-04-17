1f9a3c7ee6fff5bd2f1d9f9889ba86d9f80b3d68
Colin Bruin <colinjb23@gmail.com>
2018-04-17 12:49:28 -0400

    unknown commit

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ app.get('/', (req, res) => {
 app.post('/upload', upload.single('myFile'), function (req, res, next) {
     // req.file is the `myFile` file
     // req.body will hold the text fields, if there were any
-    console.log("Uploaded: " + req.file.filename);
     uploaded_files.push(req.file.filename);
     res.render('upload', {
         fileToUpload: req.file.filename
